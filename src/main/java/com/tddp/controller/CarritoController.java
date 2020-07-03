9a91aaa469ce20dabbc688016c7aeea7a3b11efa
paulcuevaA26 <terryca4@gmail.com>
2020-07-03 18:43:19 -0500

    controllercarrito

---
+++ src/main/java/com/tddp/controller/CarritoController.java
@@ -0,0 +1,37 @@
+package com.tddp.controller;
+
+import com.tddp.model.Carrito;
+import com.tddp.repository.CarritoRepository;
+import com.tddp.service.CarritoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CarritoController {
+
+    @Autowired
+    CarritoService carritoService;
+
+    @GetMapping("/carrito")
+    public String listCarrito(Model model){
+        model.addAttribute("carrito",carritoService.GetCarritoALL());
+        return "carrito";
+    }
+
+    @GetMapping("/carrito/add")
+    public  String addCarrito(Model model){
+        model.addAttribute("addcarrito",new Carrito());
+        return "add-carrito";
+    }
+
+    @PostMapping("/carrito/save")
+    public  String addCarritoSaveMake(Model model, Carrito carrito){
+      carritoService.createCarrito(carrito);
+      return "redirect/:carrito";
+    }
+
+
+}
